ba0b8520e63f469771c4cad64083d43254910fec
Pedro Policello <88664809+PedroPolicello@users.noreply.github.com>
2023-11-04 16:52:31 -0300

    PlayerMovement

--- ARGEndingUnity/Assets/Scripts/PlayerBehavior.cs
+++ ARGEndingUnity/Assets/Scripts/PlayerBehavior.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputs playerInputs;
+    private Vector2 moveDirection;
+
+    [SerializeField] private int speed;
+
+    private void Awake()
     {
-        
+        playerInputs = new PlayerInputs();
+
+        playerInputs.Player.Move.started += MoveHandler;
+        playerInputs.Player.Move.performed += MoveHandler;
+        playerInputs.Player.Move.canceled += MoveHandler;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MoveHandler(InputAction.CallbackContext value)
+    {
+        moveDirection = value.ReadValue<Vector2>();
+    }
+
+    void MovePlayer()
+    {
+        transform.Translate(new Vector2(moveDirection.x, moveDirection.y).normalized * speed * Time.deltaTime);
+    }
+
+
+
+
+
+
+
+
+    private void OnEnable()
+    {
+        playerInputs.Enable();   
+    }
+
+    private void OnDisable()
+    {
+        playerInputs.Disable();
     }
 }
